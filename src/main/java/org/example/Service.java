9a977ca9b7a12096bd480c22a36c70502ab4c209
abidmehboob <abidali8@hotmail.com>
2024-04-04 12:42:56 +0200

    Update Service.java

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,62 +1,12 @@
 package org.example;
 
 public class Service {
-    public boolean isEven(int input) {
-        return input % 2 == 0;
+    public static void main(String[] args) {
+        Service service = new Service();
+        service.testAssertion(); // Calling the method to trigger assertion
     }
 
-    public void highComplexityMethod(int a, int b, int c) {
-        if (a == 0) {
-            System.out.println("a is positive");
-            if (b > 0) {
-                System.out.println("b is positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            } else {
-                System.out.println("b is non-positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            }
-        } else if (a < 0) {
-            System.out.println("a is non-positive");
-            if (b > 0) {
-                System.out.println("b is positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            } else {
-                System.out.println("b is non-positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            }
-        } else {
-            System.out.println("a is non-positive");
-            if (b > 0) {
-                System.out.println("b is positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            } else {
-                System.out.println("b is non-positive");
-                if (c > 0) {
-                    System.out.println("c is positive");
-                } else {
-                    System.out.println("c is non-positive");
-                }
-            }
-        }
+    public void testAssertion() {
+        assert false : "Assertion failed";
     }
 }
