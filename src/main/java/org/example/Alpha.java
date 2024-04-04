2f172d0c0f5df9f85968fd8428427984011db6a4
abidmehboob <abidali8@hotmail.com>
2024-04-04 10:18:54 +0200

    Update Alpha.java

--- src/main/java/org/example/Alpha.java
+++ src/main/java/org/example/Alpha.java
@@ -1,5 +1,13 @@
 package org.example;
 
 public class Alpha {
+   public static void main(String[] args) {
+        Alpha alpha = new Alpha();
+        alpha.testAssertion(); // Calling the method to trigger assertion
+    }
+
+    public void testAssertion() {
+        assert false : "Assertion failed";
+    }
 
 }
