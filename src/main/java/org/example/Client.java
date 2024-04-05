7ffdc1f0f7543c19dcf97f2ed2ac7114914f3247
abidmehboob <abidali8@hotmail.com>
2024-04-05 16:16:38 +0200

    Update Client.java

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,12 +1,8 @@
 package org.example;
 
 public class Client {
-   public static void main(String[] args) {
-        Client client = new Client();
-        client.testAssertion(); // Calling the method to trigger assertion
-    }
-
-    public void testAssertion() {
-        assert false : "Assertion failed";
+  
+    public void add() {
+       int a= 2+3;
     }
 }
