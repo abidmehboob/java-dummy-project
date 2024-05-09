8155c8966ec4baace7800ac125f41de75b3e53a3
abidmehboob <abidali8@hotmail.com>
2024-05-09 11:45:34 +0200

    Update Service.java

--- src/main/java/org/example/Service.java
+++ src/main/java/org/example/Service.java
@@ -1,12 +1,7 @@
 package org.example;
 
 public class Service {
-    public static void main(String[] args) {
-        Service service = new Service();
-        service.testAssertion(); // Calling the method to trigger assertion
-    }
-
     public void testAssertion() {
-        assert false : "Assertion failed";
+       
     }
 }
