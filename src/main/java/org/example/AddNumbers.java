96e81be735546dab706a5f6128116d8f2980b0de
abmehboo <abidali8@hotmail.com>
2026-02-16 10:24:13 +0100

    Add AddNumbers.java (ANSI encoded)

---
+++ src/main/java/org/example/AddNumbers.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Small utility that provides a method to add two numbers.
+ * Implemented using double so it accepts integer or floating inputs.
+ */
+public class AddNumbers {
+    /**
+     * Return the sum of two numbers.
+     * @param a first number
+     * @param b second number
+     * @return a + b
+     */
+    public static double add(double a, double b) {
+        return a + b;
+    }
+}
