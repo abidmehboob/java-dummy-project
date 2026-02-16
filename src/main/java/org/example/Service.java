07ae4efcacafbeb536fc9213984d4157ab0b8c76
abmehboo <abidali8@hotmail.com>
2026-02-16 11:36:20 +0100

    updated "convert utf to ANSI" encoding

--- src/main/java/org/example/Service.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Service {
-    public void testAssertion() {
-       
-    }
-}
--- src/main/java/org/example/TestInterface.java
+++
@@ -1,5 +0,0 @@
-package org.example;
-
-public interface TestInterface {
-    void create(String classContainer);
-}
